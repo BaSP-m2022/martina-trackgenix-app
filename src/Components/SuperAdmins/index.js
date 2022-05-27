5f83d554bd4a82c2152b8abe81f61e7142ec17bb
Luciano Alarcon <luchialarcon123@gmail.com>
2022-05-27 19:56:42 -0300

    TG-23: branch created, fetch is working

--- src/Components/SuperAdmins/index.js
+++ src/Components/SuperAdmins/index.js
@@ -1,9 +1,28 @@
 import styles from './super-admins.module.css';
+import React, { useEffect, useState } from 'react';
 
 function SuperAdmins() {
+  const [sAdmins, saveSAdmins] = useState([]);
+  console.log(sAdmins);
+
+  useEffect(async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/super-admins`);
+      const data = await response.json();
+      saveSAdmins(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
   return (
     <section className={styles.container}>
       <h2>SuperAdmins</h2>
+      <div>
+        {sAdmins.data.map((superAd) => {
+          return <div key={superAd._id}>{superAd.firstName}</div>;
+        })}
+      </div>
     </section>
   );
 }
