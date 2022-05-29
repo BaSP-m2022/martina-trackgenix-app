1dc560f2535046bc4eb1587fc9302f464c723a14
Lucas Prat <lucasprat.cn@gmail.com>
2022-05-29 04:00:24 -0300

    TG-25: add list and delete admin functionality

---
+++ src/Components/Admins/ListItem/ListItem.js
@@ -0,0 +1,25 @@
+// import react from 'react';
+import styles from './listItem.module.css';
+
+const ListItem = ({ listItem, deleteItem }) => {
+  const handleDelete = (id) => {
+    deleteItem(id);
+  };
+  return (
+    <tr className={styles.rows}>
+      <td>{listItem._id}</td>
+      <td>{listItem.firstName}</td>
+      <td>{listItem.lastName}</td>
+      <td>{listItem.phone}</td>
+      <td>{listItem.email}</td>
+      <td>
+        <button>Edit</button>
+      </td>
+      <td>
+        <button onClick={() => handleDelete(listItem._id)}>X</button>
+      </td>
+    </tr>
+  );
+};
+
+export default ListItem;
--- src/Components/Admins/index.js
+++ src/Components/Admins/index.js
@@ -1,11 +1,30 @@
+import { useState, useEffect } from 'react';
 import styles from './admins.module.css';
+import List from './List/List';
 
-function Admins() {
+const Admins = () => {
+  const [list, setList] = useState([]);
+
+  useEffect(async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/admins`);
+      const data = await response.json();
+      setList(data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  const deleteItem = (id) => {
+    setList(list.filter((listItem) => listItem._id !== id));
+  };
+
   return (
     <section className={styles.container}>
       <h2>Admins</h2>
+      <List list={list} setList={setList} deleteItem={deleteItem} />
     </section>
   );
-}
+};
 
 export default Admins;
