fe510a8492440169bc5e5888d034855c02eddf81
unknown <eserocco97@gmail.com>
2022-05-30 12:11:36 -0300

    TG-21: Error message for add

--- src/Components/Tasks/AddForm/index.js
+++ src/Components/Tasks/AddForm/index.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
-import Modal from '../Modals';
+import Modal from '../Modals/index.js';
+import ModalError from '../Modals/error.js';
 import styles from './form.module.css';
 
 function form() {
   const [showModal, setShowModal] = useState(false);
+  const [showModalError, setShowModalError] = useState(false);
   const [userInput, setUserInput] = useState({
     description: ''
   });
@@ -32,7 +34,7 @@ function form() {
     fetch(url, option).then((response) => {
       if (response.status !== 200 && response.status !== 201) {
         return response.json().then(({ message }) => {
-          setShowModal(true);
+          setShowModalError(true);
           throw new Error(message);
         });
       }
@@ -43,10 +45,12 @@ function form() {
 
   const closeModal = () => {
     setShowModal(false);
+    setShowModalError(false);
   };
   return (
     <div className={styles.container}>
       <Modal title={'task created successfully'} show={showModal} closeModal={closeModal} />
+      <ModalError title={'There was an error'} show={showModalError} closeModal={closeModal} />
       <form onSubmit={onSubmit}>
         <div>
           <label>Description</label>
