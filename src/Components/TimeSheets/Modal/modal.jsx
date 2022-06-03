e51aefc04aed3771e5e265b617bf8820b976bb8e
Nancy Cima <nancycimabertoni@gmail.com>
2022-06-03 00:54:59 -0300

    TG-28: Corrected some details

--- src/Components/TimeSheets/Modal/modal.jsx
+++ src/Components/TimeSheets/Modal/modal.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './modal.module.css';
 
-const Modal = ({ title, setShowModal }) => {
+const Modal = ({ titleModal, showModal, setShowModal }) => {
+  if (!showModal) {
+    return null;
+  }
+
   return (
     <div className={styles.conteiner}>
-      <div className={styles.modal}>{title}</div>
+      <div className={styles.modal}>{titleModal}</div>
       <button onClick={() => setShowModal(false)} className={styles.button}>
         Close
       </button>
--- src/Components/TimeSheets/index.js
+++ src/Components/TimeSheets/index.js
@@ -1,31 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import styles from './time-sheets.module.css';
 import List from './List/List';
-import Modal from '../Modal/Modal';
+import Modal from './Modal/Modal';
 
 const TimeSheets = () => {
   const [list, setList] = useState([]);
-  const [showModal, setShowModal] = useState();
+  const [showModal, setShowModal] = useState(false);
+  const [titleModal, setTitleModal] = useState('');
+
+  const listTS = async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/time-sheet`);
+      const data = await response.json();
+      setList(data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_URL}/time-sheets`)
-      .then((response) => response.json())
-      .then((response) => {
-        setList(response.data);
-      });
+    listTS();
   }, []);
 
   const deleteItem = (_id) => {
-    setList([...list.filter((list) => list._id !== _id)]);
+    setList([...list.filter((listItem) => listItem._id !== _id)]);
   };
 
   return (
     <section className={styles.container}>
       <h2>Time-Sheets</h2>
-      {showModal && (
-        <Modal title={'Delete successfully'} show={showModal} setShowModal={setShowModal} />
-      )}
-      <List list={list} setList={setList} deleteItem={deleteItem} setShowModal={setShowModal} />
+      <List
+        list={list}
+        setList={setList}
+        deleteItem={deleteItem}
+        setTitleModal={setTitleModal}
+        setShowModal={setShowModal}
+      />
+      <Modal titleModal={titleModal} showModal={showModal} setShowModal={setShowModal} />
     </section>
   );
 };
