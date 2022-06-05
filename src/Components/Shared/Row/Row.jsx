b926209fcce9afac87b633254b5adc9b4ae6f0bc
RNDOSKO <Ricardo.dosko@gmail.com>
2022-06-05 20:31:18 -0300

    TG-34: Rows component

---
+++ src/Components/Shared/Row/Row.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './row.module.css';
+
+const Row = ({ data, headers, deleteItem, editItem, children }) => {
+  console.log('data recibida en row', data);
+  console.log('headers recibida en row', headers);
+  return (
+    <tr className={styles.tr} key={data._id}>
+      {headers.map((header, index) => {
+        return <td key={index}>{data[header]}</td>;
+      })}
+      <td>
+        {children}
+        <button onClick={editItem}>&#9998;</button>
+      </td>
+      <td>
+        <button onClick={deleteItem}>X</button>
+      </td>
+    </tr>
+  );
+};
+
+export default Row;
