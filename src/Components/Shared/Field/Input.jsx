28eb3ec71d6806c9f56f989e4bbf53422fda4d81
Luciano Alarcon <luchialarcon123@gmail.com>
2022-06-05 10:45:25 -0300

    TG-38: field shared component

---
+++ src/Components/Shared/Field/Input.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from './input.module.css';
+
+const Input = ({ children, type, name, value, onChange, identification, valueOptions }) => {
+  if (identification === 'input') {
+    return (
+      <div className={styles.container}>
+        {children}
+        <input type={type} name={name} value={value} onChange={onChange}></input>
+      </div>
+    );
+  }
+  if (identification === 'select') {
+    return (
+      <div className={styles.container}>
+        <select name={name} onChange={onChange}>
+          {valueOptions.map((item) => (
+            <option key={item._id} value={value}>
+              {item._id}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  }
+  if (identification === 'radio') {
+    return (
+      <div className={styles.container}>
+        {children}
+        <input type={type} name={name} value={value} onChange={onChange}></input>
+      </div>
+    );
+  }
+};
+
+export default Input;
