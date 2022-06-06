7702ab0cd6bbc2b6191f8105cb0347433b03cb71
Luciano Alarcon <luchialarcon123@gmail.com>
2022-06-06 11:03:37 -0300

    TG-38: fixed errors

--- src/Components/Shared/Field/RadioButton.jsx
+++ src/Components/Shared/Field/RadioButton.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import styles from './input.module.css';
 
-const RadioButton = ({ type, name, label, value, onChange }) => {
-  console.log(value);
+const RadioButton = ({ name, label, value, onChange }) => {
   return (
     <div className={styles.container}>
       <label>{label}</label>
       <div className={styles.radio}>
-        <input type={type} name={name} value={value[0]} onChange={onChange}></input>
-        <input type={type} name={name} value={value[1]} onChange={onChange}></input>
+        <input type="radio" name={name} value={value[0]} onChange={onChange}></input>
+        <input type="radio" name={name} value={value[1]} onChange={onChange}></input>
       </div>
     </div>
   );
