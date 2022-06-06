cb7abab7c8abcb0359f171bddfee210ebd866fe7
RNDOSKO <Ricardo.dosko@gmail.com>
2022-06-05 21:31:50 -0300

    TG-34: delete console log

--- src/Components/Shared/Row/Row.jsx
+++ src/Components/Shared/Row/Row.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import styles from './row.module.css';
 
 const Row = ({ data, headers, deleteItem, editItem, children }) => {
-  console.log('data recibida en row', data);
-  console.log('headers recibida en row', headers);
   return (
     <tr className={styles.tr} key={data._id}>
       {headers.map((header, index) => {
