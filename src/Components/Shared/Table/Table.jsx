dfb7a622b1ee2f29bbebaada1b5c3fa0a63701df
Nancy Cima <nancycimabertoni@gmail.com>
2022-06-08 20:57:15 -0300

    TG-47: Corrected and added features on Table

--- src/Components/Shared/Table/Table.jsx
+++ src/Components/Shared/Table/Table.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './table.module.css';
 import Row from '../Row/Row';
+import Button from '../Buttons/Buttons';
 
 const Table = ({ title, data, headers, deleteItem, editItem }) => {
-  //chieldren should be here too
-  
-  //Esto seria para que no se muestren demasiados datos en una pag
-    {// const [indexPage, setIndexPage] = useState(1);
-  //const show = data.slice(10 * (indexPage - 1), 10 * indexPage);
-  //const nextPage = () => {
-  //  if (data.length / 10 >= indexPage) {
-  //    setIndexPage(indexPage + 1);
-  //  }
-  //};
-  //const previousPage = () => {
-  //  if (indexPage > 1) {
-  //    setIndexPage(indexPage - 1);
-  //};}
-  }
+  const [indexPage, setIndexPage] = useState(1);
+  const show = data.slice(10 * (indexPage - 1), 10 * indexPage);
+  const nextPage = () => {
+    if (data.length / 10 >= indexPage) {
+      setIndexPage(indexPage + 1);
+    }
+  };
+  const previousPage = () => {
+    if (indexPage > 1) {
+      setIndexPage(indexPage - 1);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>{title}</h2>
@@ -30,37 +29,47 @@ const Table = ({ title, data, headers, deleteItem, editItem }) => {
           </tr>
         </thead>
         <tbody>
-         {//{show.map((row) => {   
-          <Row
-            data={data}
-            headers={headers}
-            deleteItem={deleteItem}
-            editItem={editItem}
-            // children={children}
-          />
+          {show.map((index) => {
+            return (
+              <Row
+                key={index}
+                data={data}
+                headers={headers}
+                deleteItem={deleteItem}
+                editItem={editItem}
+              />
+            );
+          })}
         </tbody>
       </table>
-      {// <div>
-       // <div>
-       //   <p>Page {indexPage}</p>
-       // </div>
-       // <div>
-        //  <Button
-        //    disabled={indexPage <= 1}
-        //    onClick={() => previousPage()}
-        //  >
-        //    Previous
-        //  </Button>
-        //</div>
-        //<div>
-        //  <Button
-        //    disabled={indexPage >= data.length / 10}
-        //    onClick={() => nextPage()}
-        //  >
-        //    Next
-        //  </Button>
-      //  </div>
-      //</div> }
+      <div className={styles.page}>
+        <p> Page {indexPage} </p>
+      </div>
+      <div className={styles.buttons}>
+        <div>
+          <Button
+            width={'100px'}
+            height={'40px'}
+            fontSize={'15px'}
+            margin={'2px'}
+            disabled={indexPage <= 1}
+            onClick={() => previousPage()}
+          >
+            Previous
+          </Button>
+        </div>
+        <div>
+          <Button
+            width={'100px'}
+            height={'40px'}
+            fontSize={'15px'}
+            disabled={indexPage >= data.length / 10}
+            onClick={() => nextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
