fc67de2a772d50cf7df2d94b1ec4dff005026ccf
Juan Moreira <juanmoreira1518@gmail.com>
2022-06-11 17:23:03 -0300

    TG-60: implementing getAdmins

--- src/Components/Admins/index.js
+++ src/Components/Admins/index.js
@@ -5,10 +5,27 @@ import AdminForm from './Form/AdminForm';
 import Modal from '../Shared/Modal/Modal';
 import Button from '../Shared/Buttons/Buttons';
 import Loader from '../Shared/Loader/Loader';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAdminsError, getAdminsSuccess } from '../../redux/admins/actions';
 
 const Admins = () => {
-  const [list, setList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  const getAdminsFetch = async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/admins`);
+      const data = await response.json();
+      dispatch(getAdminsSuccess(data.data));
+    } catch (error) {
+      dispatch(getAdminsError(error));
+    }
+  };
+
+  // eslint-disable-next-line prettier/prettier
+  const admins = useSelector(state => state.admins.list);
+  // eslint-disable-next-line prettier/prettier
+  const isLoading = useSelector(state => state.admins.isLoading);
+
   const [showForm, setShowForm] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [childrenModal, setChildrenModal] = useState('');
@@ -23,48 +40,37 @@ const Admins = () => {
   });
   const [method, setMethod] = useState('');
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/admins`);
-      const data = await response.json();
-      setList(data.data);
-      setIsLoading(false);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   useEffect(() => {
-    fetchData();
+    getAdminsFetch();
   }, []);
 
-  const deleteItem = (_id) => {
-    setList([...list.filter((listItem) => listItem._id !== _id)]);
-  };
+  // const deleteItem = (_id) => {
+  //   setList([...list.filter((listItem) => listItem._id !== _id)]);
+  // };
 
-  const addItem = ({ _id, firstName, lastName, phone, email, password, active }) => {
-    const newItem = {
-      _id,
-      firstName,
-      lastName,
-      phone,
-      email,
-      password,
-      active
-    };
-    setList([...list, newItem]);
-  };
+  // const addItem = ({ _id, firstName, lastName, phone, email, password, active }) => {
+  //   const newItem = {
+  //     _id,
+  //     firstName,
+  //     lastName,
+  //     phone,
+  //     email,
+  //     password,
+  //     active
+  //   };
+  //   setList([...list, newItem]);
+  // };
 
-  const editItem = (data) => {
-    const adminUpd = list.map((admin) => {
-      if (admin._id === data._id) {
-        return data;
-      } else {
-        return admin;
-      }
-    });
-    setList(adminUpd);
-  };
+  // const editItem = (data) => {
+  //   const adminUpd = list.map((admin) => {
+  //     if (admin._id === data._id) {
+  //       return data;
+  //     } else {
+  //       return admin;
+  //     }
+  //   });
+  //   setList(adminUpd);
+  // };
 
   const openForm = () => {
     setMethod('POST');
@@ -79,25 +85,25 @@ const Admins = () => {
         <section className={styles.container}>
           <h2>Admins</h2>
           <AdminForm
-            addItem={addItem}
+            // addItem={addItem}
             showForm={showForm}
             setShowForm={setShowForm}
             setShowModal={setShowModal}
             setChildrenModal={setChildrenModal}
-            setIsLoading={setIsLoading}
-            editItem={editItem}
+            // setIsLoading={setIsLoading}
+            // editItem={editItem}
             previousAdmin={previousAdmin}
             setPreviousAdmin={setPreviousAdmin}
             method={method}
           />
           <List
-            deleteItem={deleteItem}
-            list={list}
+            // deleteItem={deleteItem}
+            list={admins}
             setPreviousAdmin={setPreviousAdmin}
             setShowForm={setShowForm}
             setShowModal={setShowModal}
             setChildrenModal={setChildrenModal}
-            setIsLoading={setIsLoading}
+            // setIsLoading={setIsLoading}
             setMethod={setMethod}
           />
           <Button onClick={openForm}>Add New Admin</Button>
--- src/redux/admins/thunks.js
+++ src/redux/admins/thunks.js
@@ -1,13 +1,22 @@
 import { getAdminsPending, getAdminsSuccess, getAdminsError } from './actions';
 
+const getAdminsFetch = async () => {
+  try {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/admins`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
 export const getAdmins = () => {
   return (dispatch) => {
     dispatch(getAdminsPending());
-    return fetch(`${process.env.REACT_APP_API_URL}/admins`)
-      .then((response) => response.json())
-      .then((response) => {
-        dispatch(getAdminsSuccess(response.data));
-        return response.data;
+
+    getAdminsFetch()
+      .then((data) => {
+        dispatch(getAdminsSuccess(data));
       })
       .catch((error) => {
         dispatch(getAdminsError(error.toString()));
