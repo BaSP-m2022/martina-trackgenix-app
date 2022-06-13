6a8b1a15f9cd4e688c3a5abcd8a581ea3bdd2122
Nancy Cima <nancycimabertoni@gmail.com>
2022-06-13 19:25:37 -0300

    TG-59: Set the actions

---
+++ src/redux/projects/actions.js
@@ -0,0 +1,42 @@
+import {
+  GET_PROJECTS_PENDING,
+  GET_PROJECTS_SUCCESS,
+  GET_PROJECTS_ERROR,
+  DELETE_PROJECTS_PENDING,
+  DELETE_PROJECTS_SUCCESS,
+  DELETE_PROJECTS_ERROR
+} from './constants';
+
+export const getProjectsPending = () => {
+  return {
+    type: GET_PROJECTS_PENDING
+  };
+};
+
+export const getProjectsSuccess = (data) => {
+  return {
+    type: GET_PROJECTS_SUCCESS,
+    payload: data
+  };
+};
+
+export const getProjectsError = (error) => {
+  return {
+    type: GET_PROJECTS_ERROR,
+    payload: error
+  };
+};
+
+export const deleteProjectsPending = () => ({
+  type: DELETE_PROJECTS_PENDING
+});
+
+export const deleteProjectsSuccess = (projectsId) => ({
+  type: DELETE_PROJECTS_SUCCESS,
+  payload: projectsId
+});
+
+export const deleteProjectsError = (error) => ({
+  type: DELETE_PROJECTS_ERROR,
+  payload: error
+});
