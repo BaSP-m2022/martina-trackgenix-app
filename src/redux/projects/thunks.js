033faab556d85faa47466575f2ab8c18366e98c2
Nancy Cima <101438655+NancyCima@users.noreply.github.com>
2022-06-14 12:33:20 -0300

    TG-59: Made some corrections on thunks

--- src/redux/projects/thunks.js
+++ src/redux/projects/thunks.js
@@ -2,38 +2,36 @@ import {
   getProjectsPending,
   getProjectsSuccess,
   getProjectsError,
-  deleteProjectsSuccess,
-  deleteProjectsError,
-  deleteProjectsPending
+  deleteProjectSuccess,
+  deleteProjectError,
+  deleteProjectPending
 } from './actions';
 
-export const getProject = () => {
-  return (dispatch) => {
+export const getProjects = () => {
+  return async (dispatch) => {
     dispatch(getProjectsPending());
-    return fetch(`${process.env.REACT_APP_API_URL}/projects`)
-      .then((response) => response.json())
-      .then((response) => {
-        dispatch(getProjectsSuccess(response.data));
-        return response.data;
-      })
-      .catch((error) => {
-        dispatch(getProjectsError(error.toString()));
-      });
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/projects`);
+      const res = await response.json();
+      dispatch(getProjectsSuccess(res.data));
+      return response.data;
+    } catch (error) {
+      dispatch(getProjectsError(error.toString()));
+    }
   };
 };
 
 export const deleteProject = (_id) => {
   return async (dispatch) => {
-    if (confirm('Are you sure you want to delete this project?')) {
-      dispatch(deleteProjectsPending());
-      try {
-        await fetch(`${process.env.REACT_APP_API_URL}/projects/${_id}`, {
-          method: 'DELETE'
-        });
-        dispatch(deleteProjectsSuccess(_id));
-      } catch (error) {
-        dispatch(deleteProjectsError(error.toString()));
-      }
+    dispatch(deleteProjectPending());
+    try {
+      await fetch(`${process.env.REACT_APP_API_URL}/projects/${_id}`, {
+        method: 'DELETE'
+      });
+      dispatch(deleteProjectSuccess(_id));
+    } catch (error) {
+      dispatch(deleteProjectError(error.toString()));
     }
   };
 };
+
