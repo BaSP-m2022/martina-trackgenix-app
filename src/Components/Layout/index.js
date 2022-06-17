eadbbcf40a06b777fad7da90a08be01469f09048
Luciano Alarcon <luchialarcon123@gmail.com>
2022-06-16 21:05:26 -0300

    TG-71: implement code splitting in layout file

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-
-import Header from 'Components/Shared/Header/index';
-import NavBar from 'Components/Shared/NavBar/NavBar';
-import Footer from 'Components/Shared/Footer/index';
-import Admins from 'Components/SuperAdmin/Admins/index';
-import SuperAdmins from 'Components/SuperAdmin/SuperAdmins/index';
-import Home from 'Components/Home/index';
 import styles from './layout.module.css';
-import Employees from 'Components/SuperAdmin/Employees/index';
-import Projects from 'Components/SuperAdmin/Projects/index';
-import TimeSheets from 'Components/SuperAdmin/TimeSheets/index';
-import Tasks from 'Components/SuperAdmin/Tasks/index';
+
+const Home = lazy(() => import('Components/Home'));
+const Header = lazy(() => import('Components/Shared/Header'));
+const NavBar = lazy(() => import('Components/Shared/NavBar/NavBar'));
+const Footer = lazy(() => import('Components/Shared/Footer'));
+const Admins = lazy(() => import('Components/SuperAdmin/SuperAdmins'));
+const SuperAdmins = lazy(() => import('Components/Shared/Header'));
+const Employees = lazy(() => import('Components/SuperAdmin/Employees'));
+const Projects = lazy(() => import('Components/SuperAdmin/Projects'));
+const TimeSheets = lazy(() => import('Components/SuperAdmin/TimeSheets'));
+const Tasks = lazy(() => import('Components/SuperAdmin/Tasks'));
 
 function Layout() {
   const arrayRoute = [
@@ -24,21 +24,23 @@ function Layout() {
   ];
   return (
     <div className={styles.container}>
-      <Header />
-      <NavBar props={arrayRoute} />
-      <Switch>
-        <Route path="/home" component={Home} />
-        <Route path="/admins" component={Admins} />
-        <Route path="/super-admins" component={SuperAdmins} />
-        <Route path="/employees" component={Employees} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/time-sheets" component={TimeSheets} />
-        <Route path="/tasks" component={Tasks} />
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </Switch>
-      <Footer props={arrayRoute} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Header />
+        <NavBar props={arrayRoute} />
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/admins" component={Admins} />
+          <Route path="/super-admins" component={SuperAdmins} />
+          <Route path="/employees" component={Employees} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/time-sheets" component={TimeSheets} />
+          <Route path="/tasks" component={Tasks} />
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+        </Switch>
+        <Footer props={arrayRoute} />
+      </Suspense>
     </div>
   );
 }
