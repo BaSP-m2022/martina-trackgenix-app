c2e2412b56811fbb550f37ea09f26652ce6152e8
javierbiselli <javierbiselli@gmail.com>
2022-06-18 19:41:10 -0300

    TG-73: Restored sa form

--- src/Components/SuperAdmin/SuperAdmins/Form/SuperAdminForm.jsx
+++ src/Components/SuperAdmin/SuperAdmins/Form/SuperAdminForm.jsx
@@ -1,42 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './superAdminForm.module.css';
 import Button from '../../../Shared/Buttons/Buttons';
 import Input from '../../../Shared/Field/Input';
 import RadioButton from '../../../Shared/Field/RadioButton';
-//import { useDispatch } from 'react-redux';
-//import { addSuperAdmin, editSuperAdmin } from '../../../../redux/superAdmins/thunks';
-import { useForm } from 'react-hook-form';
-import Joi from 'joi';
-import { joiResolver } from '@hookform/resolvers/joi';
+import { useDispatch } from 'react-redux';
+import { addSuperAdmin, editSuperAdmin } from '../../../../redux/superAdmins/thunks';
 
 const SuperAdminForm = ({
   showForm,
   setShowForm,
-  //previousSuperAdmin,
-  setPreviousSuperAdmin
-  //setShowModal,
-  //setChildrenModal
+  previousSuperAdmin,
+  setPreviousSuperAdmin,
+  setShowModal,
+  setChildrenModal
 }) => {
   if (!showForm) {
     return null;
   }
 
-  //const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  const schema = Joi.object({
-    firstName: Joi.string().required().min(3).max(20)
-  });
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors }
-  } = useForm({
-    mode: 'onChange',
-    resolver: joiResolver(schema)
-  });
-
-  //const [inputSuperAdmin, setInputSuperAdmin] = useState(previousSuperAdmin);
+  const [inputSuperAdmin, setInputSuperAdmin] = useState(previousSuperAdmin);
 
   const cleanFields = () => {
     setPreviousSuperAdmin({
@@ -49,9 +33,9 @@ const SuperAdminForm = ({
     });
   };
 
-  // const onChange = (e) => {
-  //   setInputSuperAdmin({ ...inputSuperAdmin, [e.target.name]: e.target.value });
-  // };
+  const onChange = (e) => {
+    setInputSuperAdmin({ ...inputSuperAdmin, [e.target.name]: e.target.value });
+  };
 
   const onClose = () => {
     setShowForm(false);
@@ -59,73 +43,63 @@ const SuperAdminForm = ({
   };
 
   const onSubmit = async (e) => {
-    console.log(e);
-    // e.preventDefault();
-    // if (!inputSuperAdmin._id) {
-    //   const superAdminResponse = await dispatch(addSuperAdmin(inputSuperAdmin));
-    //   if (superAdminResponse.error) {
-    //     setChildrenModal(superAdminResponse.message);
-    //     setShowModal(true);
-    //   } else {
-    //     onClose();
-    //     setChildrenModal('Super Admin added');
-    //     setShowModal(true);
-    //   }
-    // } else {
-    //   const superAdminResponse = await dispatch(editSuperAdmin(inputSuperAdmin));
-    //   if (superAdminResponse.error) {
-    //     setChildrenModal(superAdminResponse.message);
-    //     setShowModal(true);
-    //   } else {
-    //     onClose();
-    //     setChildrenModal('Super Admin edited');
-    //     setShowModal(true);
-    //   }
-    // }
+    e.preventDefault();
+    if (!inputSuperAdmin._id) {
+      const superAdminResponse = await dispatch(addSuperAdmin(inputSuperAdmin));
+      if (superAdminResponse.error) {
+        setChildrenModal(superAdminResponse.message);
+        setShowModal(true);
+      } else {
+        onClose();
+        setChildrenModal('Super Admin added');
+        setShowModal(true);
+      }
+    } else {
+      const superAdminResponse = await dispatch(editSuperAdmin(inputSuperAdmin));
+      if (superAdminResponse.error) {
+        setChildrenModal(superAdminResponse.message);
+        setShowModal(true);
+      } else {
+        onClose();
+        setChildrenModal('Super Admin edited');
+        setShowModal(true);
+      }
+    }
   };
 
   return (
     <div className={styles.container}>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={onSubmit}>
         <h2>Form Edit</h2>
         <Input
           type={'text'}
           name={'firstName'}
-          label={'First name'}
-          htmlFor={'firstName'}
-          register={register}
-          error={errors.firstName?.message}
+          value={inputSuperAdmin.firstName}
+          onChange={onChange}
+          label={'Name'}
         />
         <Input
           type={'text'}
           name={'lastName'}
-          label={'Last name'}
-          htmlFor={'lastName'}
-          register={register}
-          error={errors.lastName?.message}
+          value={inputSuperAdmin.lastName}
+          onChange={onChange}
+          label={'Last Name'}
         />
         <Input
           type={'text'}
           name={'email'}
+          value={inputSuperAdmin.email}
+          onChange={onChange}
           label={'Email'}
-          htmlFor={'email'}
-          register={register}
-          error={errors.email?.message}
         />
         <Input
           type={'password'}
           name={'password'}
+          value={inputSuperAdmin.password}
+          onChange={onChange}
           label={'Password'}
-          htmlFor={'password'}
-          register={register}
-          error={errors.password?.message}
         />
-        <RadioButton
-          name={'active'}
-          label={'Active'}
-          valueOptions={[true, false]}
-          register={register}
-        />
+        <RadioButton name="active" label={'Active'} value={[true, false]} onChange={onChange} />
         <Button onClick={(e) => onSubmit(e)}>Confirm</Button>
         <div>
           <Button onClick={onClose}> Close </Button>
