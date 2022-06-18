91b2867710be1bdcb27cb55e504a4e76e55e0e48
Javier Biselli <javierbiselli@gmail.com>
2022-06-18 19:22:02 -0300

    TG-73: Deleted comments, deleted htmlFor prop

--- src/Components/Shared/Field/Input.jsx
+++ src/Components/Shared/Field/Input.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import styles from './input.module.css';
 
-const Input = ({ type, name, label, register, valueOptions, error, htmlFor }) => {
+const Input = ({ type, name, label, register, valueOptions, error }) => {
   return (
     <>
       {type === 'select' ? (
         <div className={styles.container}>
-          <label htmlFor={htmlFor}>{label}</label>
+          <label htmlFor={name}>{label}</label>
           <select name={name} {...register(name)}>
-            {/* not sure if necessary:
-            {!value ? <option>Select one</option> : <option>{value}</option>} */}
             {valueOptions.map((item) => (
               <option key={item._id} value={item._id}>
                 {item._id} - {item.first_name || item.project_name || item.description}
@@ -20,7 +18,7 @@ const Input = ({ type, name, label, register, valueOptions, error, htmlFor }) =>
         </div>
       ) : (
         <div className={styles.container}>
-          <label htmlFor={htmlFor}>{label}</label>
+          <label htmlFor={name}>{label}</label>
           <input
             type={type}
             label={label}
