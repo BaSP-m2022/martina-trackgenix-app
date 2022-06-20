f4c6b27c4ba33e3574f04a7652d8e99eccaea964
Nancy Cima <nancycimabertoni@gmail.com>
2022-06-20 19:32:14 -0300

    TG-74: Added an admin schema and validations with joi

--- src/Components/SuperAdmin/Admins/Form/AdminForm.jsx
+++ src/Components/SuperAdmin/Admins/Form/AdminForm.jsx
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
-import styles from 'Components/SuperAdmin/Admins/Form/adminForm.module.css';
+import React from 'react';
+import styles from './adminForm.module.css';
 import Input from 'Components/Shared/Field/Input';
 import RadioButton from 'Components/Shared/Field/RadioButton';
 import Button from 'Components/Shared/Buttons/Buttons';
 import { useDispatch } from 'react-redux/es/exports';
 import { addAdmin, editAdmin } from 'redux/admins/thunks';
+import { joiResolver } from '@hookform/resolvers/joi';
+import Joi from 'joi';
+import { useForm } from 'react-hook-form';
 
+const adminSchema = Joi.object({
+  firstName: Joi.string().min(3).max(15).required(),
+  lastName: Joi.string().min(3).max(15).required(),
+  phone: Joi.number().min(10).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().alphanum().min(8).required(),
+  active: Joi.boolean().required()
+});
 const AdminForm = ({
   showForm,
   setShowForm,
@@ -20,7 +31,14 @@ const AdminForm = ({
 
   const dispatch = useDispatch();
 
-  const [userInput, setUserInput] = useState(previousAdmin);
+  const {
+    handleSubmit,
+    register,
+    formState: { errors }
+  } = useForm({
+    mode: 'onChange',
+    resolver: joiResolver(adminSchema)
+  });
 
   const cleanFields = () => {
     setPreviousAdmin({
@@ -34,15 +52,11 @@ const AdminForm = ({
     });
   };
 
-  const onChange = (e) => {
-    setUserInput({ ...userInput, [e.target.name]: e.target.value });
-  };
-
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    if (!userInput._id) {
-      const adminResponse = await dispatch(addAdmin(userInput));
+    if (!previousAdmin._id) {
+      const adminResponse = await dispatch(addAdmin(previousAdmin));
       if (adminResponse.error) {
         setChildrenModal(adminResponse.message);
         setShowModal(true);
@@ -50,7 +64,7 @@ const AdminForm = ({
         closeForm();
       }
     } else {
-      const adminResponse = await dispatch(editAdmin(userInput));
+      const adminResponse = await dispatch(editAdmin(previousAdmin));
       if (adminResponse.error) {
         setChildrenModal(adminResponse.message);
         setShowModal(true);
@@ -67,58 +81,52 @@ const AdminForm = ({
 
   return (
     <div className={styles.container}>
-      <form onSubmit={onSubmit}>
-        <h2>Add new admin</h2>
-        <div>
-          <Input
-            type="text"
-            name="firstName"
-            label={'First name'}
-            value={userInput.firstName}
-            onChange={onChange}
-          />
-        </div>
-        <div>
-          <Input
-            type="text"
-            name="lastName"
-            label={'Last name'}
-            value={userInput.lastName}
-            onChange={onChange}
-          />
-        </div>
-        <div>
-          <Input
-            type="text"
-            name="phone"
-            label={'Phone'}
-            value={userInput.phone}
-            onChange={onChange}
-          />
-        </div>
-        <div>
-          <Input
-            type="email"
-            name="email"
-            label={'Email'}
-            value={userInput.email}
-            onChange={onChange}
-          />
-        </div>
-        <div>
-          <Input
-            type="password"
-            name="password"
-            label={'Password'}
-            value={userInput.password}
-            onChange={onChange}
-          />
-        </div>
-        <div>
-          <RadioButton name="active" label={'Active'} value={[true, false]} onChange={onChange} />
-        </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {!previousAdmin._id ? <h2>Add a new admin</h2> : <h2>Edit admin</h2>}
+        <Input
+          type={'text'}
+          name={'firstName'}
+          label={'First name'}
+          register={register}
+          error={errors.firstName?.message}
+        />
+        <Input
+          type={'text'}
+          name={'lastName'}
+          label={'Last Name'}
+          register={register}
+          error={errors.lastName?.message}
+        />
+        <Input
+          type={'text'}
+          name={'phone'}
+          label={'Phone'}
+          register={register}
+          error={errors.phone?.message}
+        />
+        <Input
+          type={'email'}
+          name={'email'}
+          label={'Email'}
+          register={register}
+          error={errors.email?.message}
+        />
+        <Input
+          type={'password'}
+          name={'password'}
+          label={'Password'}
+          register={register}
+          error={errors.password?.message}
+        />
+        <RadioButton
+          name="active"
+          label={'Active'}
+          valueOptions={[true, false]}
+          register={register}
+          error={errors.active?.message}
+        />
         <div className={styles.button}>
-          <Button onClick={(e) => onSubmit(e)}>Submit</Button>
+          <Button onClick={handleSubmit(onSubmit)}>Submit</Button>
           <Button onClick={closeForm}>Close</Button>
         </div>
       </form>
