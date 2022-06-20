f95222f80c540d6d7fd33ced9f9c02d63cd78c21
SebastianCocci <sebacocci@gmail.com>
2022-06-20 19:59:51 -0300

    TG-81: My project employee.

---
+++ src/Components/Employee/Projects/index.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import Table from 'Components/Shared/Table/Table';
+import { useDispatch, useSelector } from 'react-redux';
+import { getProjects } from 'redux/projects/thunks';
+
+const Projects = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getProjects());
+  }, []);
+  let listProject = useSelector((state) => state.projects.list);
+  const employeeId = '629d41966737e327d3189242';
+  listProject = listProject.filter((project) => project.employees[0].id == employeeId);
+  console.log(listProject);
+  return (
+    <Table
+      title={'Projects'}
+      data={listProject}
+      headersColumns={['ID', 'Project Name', 'Client', 'Start Date', 'Finish Date']}
+      headers={['_id', 'project_name', 'client', 'start_date', 'finish_date']}
+    />
+  );
+};
+
+export default Projects;
