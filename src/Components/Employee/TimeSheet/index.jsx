7a9243fa55757c43bbb384a14b7a30051e9f8083
Juan Moreira <juanmoreira1518@gmail.com>
2022-06-21 19:07:43 -0300

    TG-82: get filtered timesheets done

--- src/Components/Employee/TimeSheet/index.jsx
+++ src/Components/Employee/TimeSheet/index.jsx
@@ -2,34 +2,37 @@ import React, { useEffect } from 'react';
 import Table from 'Components/Shared/Table/Table';
 import { useDispatch, useSelector } from 'react-redux';
 import { getTimeSheet } from 'redux/timeSheets/thunks';
-import { getEmployees } from 'redux/employees/thunks';
 
 const timeSheet = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getTimeSheet());
-    dispatch(getEmployees());
   }, []);
 
-  let listTimeSheet = useSelector((state) => state.timeSheet.list);
+  const listTimeSheet = useSelector((state) => state.timeSheet.list);
   const employeeId = '629d41966737e327d3189242';
 
-  // const employee = useSelector((state) => state.employees.list).find(
-  //   (employee) => employee._id === '629d41966737e327d3189242'
-  // );
-  // console.log('Employee: ', employee);
+  const filteredListTimeSheet = listTimeSheet.filter(
+    (timeSheet) => timeSheet.employee._id == employeeId
+  );
 
-  console.log('List 1: ', listTimeSheet);
-
-  listTimeSheet = listTimeSheet.filter((timeSheet) => timeSheet.employee._id == employeeId);
-  console.log('List 2: ', listTimeSheet);
+  const newList = filteredListTimeSheet.map((item) => {
+    return {
+      _id: item._id,
+      employee: item.employee ? item.employee.first_name : '',
+      hs_worked: item.hs_worked,
+      task: item.task ? item.task.description : '',
+      project: item.project ? item.project.project_name : '',
+      timesheetDate: item.timesheetDate.slice(0, 10)
+    };
+  });
 
   return (
     <Table
-      // title={`${employee.first_name} ${employee.last_name} Time-Sheet`}
+      // title={`${newList ? newList[0].employee : ''} Time-Sheet`}
       title={'<Employee-Name> TimeSheet'}
-      data={listTimeSheet}
+      data={newList}
       headersColumns={['ID', 'Employee', 'Hours Worked', 'Project', 'Task', 'Date']}
       headers={['_id', 'employee', 'hs_worked', 'project', 'task', 'timesheetDate']}
     />
