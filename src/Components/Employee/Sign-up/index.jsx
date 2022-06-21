1675237db383b6e004cc4c5c28d640de1878a455
Luciano Alarcon <luchialarcon123@gmail.com>
2022-06-20 22:34:53 -0300

    TG-84: form sign-up created whit react hook form

---
+++ src/Components/Employee/Sign-up/index.jsx
@@ -0,0 +1,137 @@
+import React, { useState } from 'react';
+import styles from 'Components/Employee/Sign-up/sign-up.module.css';
+import Input from 'Components/Shared/Field/Input';
+import Button from 'Components/Shared/Buttons/Buttons';
+import RadioButton from 'Components/Shared/Field/RadioButton';
+import { useForm } from 'react-hook-form';
+import joi from 'joi';
+import { joiResolver } from '@hookform/resolvers/joi';
+import { useDispatch } from 'react-redux';
+import { addEmployee } from 'redux/employees/thunks';
+import Modal from '../../Shared/Modal/Modal';
+
+const SingUpEmployee = () => {
+  const [userInput] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [childrenModal, setChildrenModal] = useState('');
+  const dispatch = useDispatch();
+
+  const schema = joi.object({
+    first_name: joi.string().min(3).max(30).required(),
+    last_name: joi.string().min(3).max(30).required(),
+    phone: joi.number().min(7).required(),
+    email: joi
+      .string()
+      .email({ tlds: { allow: false } })
+      .required(),
+    password: joi.string().min(6).required(),
+    active: joi.boolean().required()
+  });
+
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors }
+  } = useForm({
+    mode: 'onChange',
+    resolver: joiResolver(schema),
+    defaultValues: {
+      first_name: userInput.first_name,
+      last_name: userInput.last_name,
+      phone: userInput.phone,
+      email: userInput.email,
+      password: userInput.password,
+      active: userInput.active
+    }
+  });
+
+  const onSubmit = async (data) => {
+    try {
+      const employee = await dispatch(addEmployee(data));
+      if (employee.error) {
+        setChildrenModal(employee.message);
+        setShowModal(true);
+      } else {
+        setChildrenModal(employee.message);
+        setShowModal(true);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <section className={styles.container}>
+      <Modal isOpen={showModal} handleClose={() => setShowModal(false)}>
+        {childrenModal}
+      </Modal>
+      <div className={styles.containerForm}>
+        <h2>Sign-Up</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <Input
+              type={'text'}
+              name={'first_name'}
+              label={'First Name'}
+              register={register}
+              error={errors.first_name?.message}
+            />
+          </div>
+          <div>
+            <Input
+              type={'text'}
+              name={'last_name'}
+              label={'Last Name'}
+              register={register}
+              error={errors.last_name?.message}
+            />
+          </div>
+          <div>
+            <Input
+              type={'text'}
+              name={'phone'}
+              label={'Phone'}
+              register={register}
+              error={errors.phone?.message}
+            />
+          </div>
+          <div>
+            <Input
+              type={'text'}
+              name={'email'}
+              label={'Email'}
+              register={register}
+              error={errors.email?.message}
+            />
+          </div>
+          <div>
+            <Input
+              type={'password'}
+              name={'password'}
+              label={'Password'}
+              register={register}
+              error={errors.password?.message}
+            />
+          </div>
+          <div>
+            <RadioButton
+              name={'active'}
+              label={'Active'}
+              valueOptions={[true, false]}
+              register={register}
+              error={errors.active?.message}
+            />
+          </div>
+          <div className={styles.containerButtons}>
+            <Button onSubmit={handleSubmit(onSubmit)}>Submit</Button>
+            <Button>Close</Button>
+            <Button onClick={() => reset}>Reset Form</Button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default SingUpEmployee;
--- src/Components/Shared/index.js
+++ src/Components/Shared/index.js
@@ -1,9 +1,9 @@
-import Button from './Buttons';
+import Button from './Buttons/Buttons';
 import Input from './Field/Input';
 import RadioButton from './Field/RadioButton';
-import Loader from './Loader';
-import Modal from './Modal';
-import Row from './Row';
-import Table from './Table';
+import Loader from './Loader/Loader';
+import Modal from './Modal/Modal';
+import Row from './Row/Row';
+import Table from './Table/Table';
 
 export { Button, Input, RadioButton, Loader, Modal, Row, Table };
