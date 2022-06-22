3ed6af11a16c9be2bbbcd9e769604be96575387d
Nancy Cima <nancycimabertoni@gmail.com>
2022-06-21 21:26:03 -0300

    TG-77: Implemented the table component

--- src/Components/SuperAdmin/Employees/List/List.jsx
+++ src/Components/SuperAdmin/Employees/List/List.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import styles from 'Components/SuperAdmin/Employees/List/List.module.css';
-import Row from 'Components/Shared/Row/Row';
+import Table from 'Components/Shared/Table/Table';
 import { useSelector, useDispatch } from 'react-redux/es/exports';
 import { deleteEmployee } from 'redux/employees/thunks';
 
@@ -20,32 +19,14 @@ const List = ({ setShowForm, setPreviewsEmployee }) => {
   };
 
   return (
-    <section className={styles.container}>
-      <table>
-        <thead>
-          <tr>
-            <th id="id">ID</th>
-            <th id="first_name">First Name</th>
-            <th id="last_name">Last Name</th>
-            <th id="phone">Phone</th>
-            <th id="email">Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {employees.map((item) => {
-            return (
-              <Row
-                key={item._id}
-                data={item}
-                headers={['_id', 'first_name', 'last_name', 'phone', 'email']}
-                deleteItem={() => handleDelete(item._id)}
-                editItem={() => handleEdit(item)}
-              />
-            );
-          })}
-        </tbody>
-      </table>
-    </section>
+    <Table
+      title={'Employees'}
+      data={employees}
+      headersColumns={['ID', 'Fist Name', 'Last Name', 'Phone', 'Email']}
+      headers={['_id', 'first_name', 'last_name', 'phone', 'email']}
+      deleteItem={handleDelete}
+      editItem={handleEdit}
+    />
   );
 };
 
