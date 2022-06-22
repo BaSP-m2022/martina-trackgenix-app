2af2fb46c54ac753d4af8f877ee61ae687821ba2
Juan Moreira <juanmoreira1518@gmail.com>
2022-06-21 23:44:14 -0300

    TG-82: add new time-sheet form WIP

---
+++ src/Components/Employee/TimeSheet/Form/Form.jsx
@@ -0,0 +1,204 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTimeSheet, editTimeSheet } from 'redux/timeSheets/thunks';
+import styles from './form.module.css';
+import Button from 'Components/Shared/Buttons/Buttons';
+import Input from 'Components/Shared/Field/Input';
+import { joiResolver } from '@hookform/resolvers/joi';
+import Joi from 'joi';
+import { useForm } from 'react-hook-form';
+
+const Form = ({
+  showForm,
+  setShowForm,
+  setShowModal,
+  setChildrenModal,
+  previousTimeSheet,
+  setPreviousTimeSheet
+}) => {
+  if (!showForm) {
+    return null;
+  }
+
+  const dispatch = useDispatch();
+  const schema = Joi.object({
+    employee: Joi.string().required().length(24).alphanum(),
+    project: Joi.string().required().length(24).alphanum(),
+    task: Joi.string().required().length(24).alphanum(),
+    hsWorked: Joi.number().required(),
+    timesheetDate: Joi.date().required().greater('01-01-1950').less('now')
+  });
+  const [listProjects, setListProjects] = useState([]);
+  const [listTasks, setListTasks] = useState([]);
+
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors }
+  } = useForm({
+    mode: 'onChange',
+    resolver: joiResolver(schema),
+    defaultValue: {
+      employee: previousTimeSheet.employee,
+      project: previousTimeSheet.project,
+      task: previousTimeSheet.task,
+      hsWorked: previousTimeSheet.hs_worked,
+      timesheetDate: previousTimeSheet.timesheetDate
+    }
+  });
+
+  const fetchProjects = async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/projects`);
+      const data = await response.json();
+      setListProjects(...listProjects, data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
+  const projectName = listProjects.map((item) => {
+    if (item._id == previousTimeSheet.project) {
+      return item.project_name;
+    }
+  });
+
+  const fetchTasks = async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/tasks`);
+      const data = await response.json();
+      setListTasks(...listTasks, data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const taskDescription = listTasks.map((item) => {
+    if (item._id == previousTimeSheet.task) {
+      return item.description;
+    }
+  });
+
+  const onSubmit = async (data) => {
+    const newTimeSheet = {
+      _id: previousTimeSheet._id,
+      employee: {
+        _id: previousTimeSheet.employee,
+        first_name: 'Homero'
+      },
+      project: {
+        _id: data.project,
+        project_name: projectName
+      },
+      task: {
+        _id: data.task,
+        description: taskDescription
+      },
+      hs_worked: data.hsWorked,
+      timesheetDate: data.timesheetDate.toString()
+    };
+
+    if (!previousTimeSheet._id) {
+      try {
+        const timeSheetResponse = await dispatch(addTimeSheet(newTimeSheet));
+        if (timeSheetResponse.error) {
+          setChildrenModal(timeSheetResponse.message);
+          setShowModal(true);
+        } else {
+          setChildrenModal(timeSheetResponse.message);
+          setShowModal(true);
+          closeForm();
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      try {
+        const timeSheetResponse = await dispatch(editTimeSheet(newTimeSheet));
+        if (timeSheetResponse.error) {
+          setChildrenModal(timeSheetResponse.message);
+          setShowModal(true);
+        } else {
+          setChildrenModal(timeSheetResponse.message);
+          setShowModal(true);
+          closeForm();
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  const closeForm = () => {
+    setPreviousTimeSheet({
+      _id: '',
+      hs_worked: 0,
+      task: '',
+      project: '',
+      timesheetDate: ''
+    });
+    setShowForm(false);
+  };
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h2>Edit time-sheet</h2>
+        <div>
+          <Input
+            type={'select'}
+            name={'project'}
+            register={register}
+            valueOptions={listProjects}
+            label={'Select a Project'}
+            error={errors.project?.message}
+          ></Input>
+        </div>
+        <div>
+          <Input
+            type={'select'}
+            name={'task'}
+            register={register}
+            valueOptions={listTasks}
+            label={'Select a Task'}
+            error={errors.task?.message}
+          ></Input>
+        </div>
+        <div>
+          <Input
+            type={'number'}
+            name={'hsWorked'}
+            register={register}
+            label={'Worked Hours'}
+            error={errors.hsWorked?.message}
+          ></Input>
+        </div>
+        <div>
+          <Input
+            type={'date'}
+            name={'timesheetDate'}
+            register={register}
+            label={'DATE'}
+            error={errors.timesheetDate?.message}
+          ></Input>
+        </div>
+        <div className={styles.button}>
+          <Button onClick={handleSubmit(onSubmit)}>Submit</Button>
+          <Button onClick={() => reset()}>Reset Form</Button>
+          <Button onClick={closeForm}>Close</Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
--- src/Components/Employee/TimeSheet/index.jsx
+++ src/Components/Employee/TimeSheet/index.jsx
@@ -1,17 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Table from 'Components/Shared/Table/Table';
+import Modal from 'Components/Shared/Modal/Modal';
+import Form from './Form/Form';
+import Button from 'Components/Shared/Buttons/Buttons';
 import { useDispatch, useSelector } from 'react-redux';
 import { getTimeSheet } from 'redux/timeSheets/thunks';
 
 const timeSheet = () => {
   const dispatch = useDispatch();
 
+  const employeeId = '629d41966737e327d3189242';
+
+  const [showModal, setShowModal] = useState(false);
+  const [childrenModal, setChildrenModal] = useState('');
+  const [showForm, setShowForm] = useState(false);
+  const [previousTimeSheet, setPreviousTimeSheet] = useState({
+    _id: '',
+    employee: employeeId,
+    hs_worked: 0,
+    task: '',
+    project: '',
+    timesheetDate: ''
+  });
+
   useEffect(() => {
     dispatch(getTimeSheet());
   }, []);
 
   const listTimeSheet = useSelector((state) => state.timeSheet.list);
-  const employeeId = '629d41966737e327d3189242';
 
   const filteredListTimeSheet = listTimeSheet.filter(
     (timeSheet) => timeSheet.employee?._id == employeeId
@@ -29,12 +45,27 @@ const timeSheet = () => {
   });
 
   return (
-    <Table
-      title={`${newList[0] ? newList[0].employee : ''}'s Time-Sheet`}
-      data={newList}
-      headersColumns={['ID', 'Employee', 'Hours Worked', 'Project', 'Task', 'Date']}
-      headers={['_id', 'employee', 'hs_worked', 'project', 'task', 'timesheetDate']}
-    />
+    <section>
+      <Table
+        title={`${newList[0] ? newList[0].employee : ''}'s Time-Sheet`}
+        data={newList}
+        headersColumns={['ID', 'Employee', 'Hours Worked', 'Project', 'Task', 'Date']}
+        headers={['_id', 'employee', 'hs_worked', 'project', 'task', 'timesheetDate']}
+      />
+
+      <Form
+        showForm={showForm}
+        setShowForm={setShowForm}
+        setShowModal={setShowModal}
+        setChildrenModal={setChildrenModal}
+        previousTimeSheet={previousTimeSheet}
+        setPreviousTimeSheet={setPreviousTimeSheet}
+      />
+      <Button onClick={() => setShowForm(true)}>Add a TimeSheets</Button>
+      <Modal isOpen={showModal} handleClose={() => setShowModal(false)}>
+        {childrenModal}
+      </Modal>
+    </section>
   );
 };
 
