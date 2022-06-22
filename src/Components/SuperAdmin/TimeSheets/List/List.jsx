a32a00f1a21747f243b4e33aa63ea6da06e3a36f
unknown <eserocco97@gmail.com>
2022-06-22 11:15:36 -0300

    TG-78: path corrections

--- src/Components/SuperAdmin/TimeSheets/List/List.jsx
+++ src/Components/SuperAdmin/TimeSheets/List/List.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './list.module.css';
+import styles from 'Components/SuperAdmin/TimeSheets/List/list.module.css';
 import Row from 'Components/Shared/Row/Row';
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteTimeSheet } from 'redux/timeSheets/thunks';
