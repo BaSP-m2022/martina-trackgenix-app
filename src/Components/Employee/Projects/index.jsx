d4bbc1436aca9682fa473a165ef44afcf85a6127
SebastianCocci <sebacocci@gmail.com>
2022-06-24 10:05:37 -0300

    TG-81: Add the modal for the edit and delete.

--- src/Components/Employee/Projects/index.jsx
+++ src/Components/Employee/Projects/index.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Table from 'Components/Shared/Table/Table';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProjects } from 'redux/projects/thunks';
 import Loader from 'Components/Shared/Loader/Loader';
+import Modal from 'Components/Shared/Modal/Modal';
 
 const Projects = () => {
   const isLoading = useSelector((state) => state.projects.isLoading);
   const dispatch = useDispatch();
+  const [showModal, setShowModal] = useState(false);
+  const [childrenModal, setChildrenModal] = useState('');
 
   useEffect(() => {
     dispatch(getProjects());
@@ -17,17 +20,35 @@ const Projects = () => {
   const listProjectEmployee = listProject.filter(
     (project) => project.employees[0].id == employeeId
   );
+
+  const handleDelete = () => {
+    setShowModal(true);
+    setChildrenModal('You cannot delete project');
+  };
+
+  const handleEdit = () => {
+    setShowModal(true);
+    setChildrenModal('You cannot edit project');
+  };
+
   return (
     <>
       {isLoading ? (
         <Loader show={true} />
       ) : (
-        <Table
-          title={'My Projects'}
-          data={listProjectEmployee}
-          headersColumns={['ID', 'Project Name', 'Client', 'Start Date', 'Finish Date']}
-          headers={['_id', 'project_name', 'client', 'start_date', 'finish_date']}
-        />
+        <section>
+          <Table
+            title={'My Projects'}
+            data={listProjectEmployee}
+            headersColumns={['ID', 'Project Name', 'Client', 'Start Date', 'Finish Date']}
+            headers={['_id', 'project_name', 'client', 'start_date', 'finish_date']}
+            deleteItem={handleDelete}
+            editItem={handleEdit}
+          />
+          <Modal isOpen={showModal} handleClose={() => setShowModal(false)}>
+            {childrenModal}
+          </Modal>
+        </section>
       )}
       ;
     </>
