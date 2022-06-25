14b097a01ce1b798e4a2278fef8eb3fd079065ae
unknown <eserocco97@gmail.com>
2022-06-25 18:50:38 -0300

    TG-94: Reducer to authenticate

---
+++ src/redux/auth/reducer.js
@@ -0,0 +1,56 @@
+import {
+  LOGIN_PENDING,
+  LOGIN_SUCCESS,
+  LOGIN_ERROR,
+  CLEAN_ERROR,
+  SET_AUTHENTICATION
+} from 'redux/auth/constants';
+
+const initialState = {
+  isLoading: false,
+  authenticated: false,
+  error: ''
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN_PENDING: {
+      return {
+        ...state,
+        isLoading: true,
+        error: initialState.error
+      };
+    }
+    case LOGIN_SUCCESS: {
+      return {
+        ...state,
+        isLoading: false,
+        authenticated: true
+      };
+    }
+    case LOGIN_ERROR: {
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+    }
+    case CLEAN_ERROR: {
+      return {
+        ...state,
+        error: initialState.error
+      };
+    }
+    case SET_AUTHENTICATION: {
+      return {
+        ...state,
+        authenticated: true
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export default authReducer;
