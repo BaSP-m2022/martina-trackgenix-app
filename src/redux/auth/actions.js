d95ae36ec1b481eb2f85760730753a67b527cfde
unknown <eserocco97@gmail.com>
2022-06-25 16:34:38 -0300

    TG-94: creating actions to authenticate

---
+++ src/redux/auth/actions.js
@@ -0,0 +1,39 @@
+import {
+  LOGIN_PENDING,
+  LOGIN_SUCCESS,
+  LOGIN_ERROR,
+  CLEAN_ERROR,
+  SET_AUTHENTICATION
+} from 'redux/auth/constants';
+
+export const loginPending = () => {
+  return {
+    type: LOGIN_PENDING
+  };
+};
+
+export const loginSuccess = (data) => {
+  return {
+    type: LOGIN_SUCCESS,
+    payload: data
+  };
+};
+
+export const loginError = (error) => {
+  return {
+    type: LOGIN_ERROR,
+    payload: error
+  };
+};
+
+export const cleanError = () => {
+  return {
+    type: CLEAN_ERROR
+  };
+};
+
+export const setAuthentication = () => {
+  return {
+    type: SET_AUTHENTICATION
+  };
+};
