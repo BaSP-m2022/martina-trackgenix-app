777180261791c2ab6ebf1b3d1dd10d0775e923e2
Luciano Alarcon <luchialarcon123@gmail.com>
2022-06-25 22:23:36 -0300

    TG-86: change sensitive info to .env variables

--- src/helper/firebase/index.js
+++ src/helper/firebase/index.js
@@ -3,12 +3,12 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDvmi-IhN0_qIbKNXXmJD2Qg35hf-7jzts',
-  authDomain: 'basp-2022-trackgenix.firebaseapp.com',
-  projectId: 'basp-2022-trackgenix',
-  storageBucket: 'basp-2022-trackgenix.appspot.com',
-  messagingSenderId: '537447283020',
-  appId: '1:537447283020:web:1b0b8b34a59fd26e6577e9'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
