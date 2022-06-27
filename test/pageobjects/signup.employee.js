141b76b7dfb6dbf7626fe7604ad168ef2718b4cf
Shirley1702 <shirleyseaton17@gmail.com>
2022-06-27 01:11:33 -0300

    TG-101: start with the e2e of the sign up for the employee

--- test/pageobjects/signup.employee.js
+++ test/pageobjects/signup.employee.js
@@ -1,2 +1,49 @@
-class signupEmployee {}
+/* eslint-disable no-undef */
+class signupEmployee {
+  get bodySignup() {
+    return $('#root > div > div');
+  }
+  get navSignUp() {
+    return $('#root > div > div > nav');
+  }
+  get trackgenixNav() {
+    return $('#root > div > div > nav > a');
+  }
+  get navBarRutes() {
+    return $('#root > div > div > nav > ul');
+  }
+  get formSignup() {
+    return $('section > div > form > div:nth-child(1) > div');
+  }
+  get firtName() {
+    return $('section > div > form > div:nth-child(1) > div');
+  }
+  get lastName() {
+    return $('section > div > form > div:nth-child(2) > div');
+  }
+  get phone() {
+    return $('section > div > form > div:nth-child(3) > div');
+  }
+  get email() {
+    return $('section > div > form > div:nth-child(4) > div');
+  }
+  get password() {
+    return $('section > div > form > div:nth-child(5) > div');
+  }
+  get InputFirtName() {
+    return $('form > div:nth-child(1) > div > input');
+  }
+  get InputLastName() {
+    return $('form > div:nth-child(2) > div > input');
+  }
+  get InputPhone() {
+    return $('form > div:nth-child(3) > div > input');
+  }
+  get InputEmail() {
+    return $('form > div:nth-child(3) > div > input');
+  }
+  get InputPassword() {
+    return $('form > div:nth-child(4) > div > input');
+  }
+}
 module.exports = new signupEmployee();
--- test/specs/signup.employee.js
+++ test/specs/signup.employee.js
@@ -1,2 +1,50 @@
-/* const HomePage = require('../pageobjects/home.page');
-const SignupEmployee = require('../pageobjects/signup.employee'); */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+const HomePage = require('../pageobjects/home.page');
+const SignupEmployee = require('../pageobjects/signup.employee');
+
+describe('Sign up testing', () => {
+  beforeAll('open browser', () => {
+    browser.url('https://martina-trackgenix-app.vercel.app/employee/sign-up');
+  }),
+    it('Wait for header: title, socialRed', async () => {
+      await expect(HomePage.headerHome).toBeDisplayed();
+      await expect(HomePage.titleHome).toBeDisplayed();
+      await expect(HomePage.titleHome).toHaveText('Radium Rocket');
+      await expect(HomePage.socialRedHome).toBeDisplayed();
+      await expect(HomePage.facebookSocialRed).toBeDisplayed();
+      await expect(HomePage.twitterSocialRed).toBeDisplayed();
+      await expect(HomePage.instagramSocialRed).toBeDisplayed();
+    });
+  it('SocialRed btn to be clickable', async () => {
+    await expect(HomePage.facebookSocialRed).toBeClickable();
+    await expect(HomePage.twitterSocialRed).toBeClickable();
+    await expect(HomePage.instagramSocialRed).toBeClickable();
+  });
+  it('Each socialRed btn must have your image', async () => {
+    await expect(HomePage.imgFacebook).toHaveAttrContaining('src', '/assets/images/facebook.svg');
+    await expect(HomePage.imgTwitter).toHaveAttrContaining('src', '/assets/images/twitter.svg');
+    await expect(HomePage.imgInstagram).toHaveAttrContaining('src', '/assets/images/instagram.svg');
+  });
+  it('Each socialRed btn must have your href', async () => {
+    await expect(HomePage.facebookSocialRed).toHaveAttribute('href', false);
+    await expect(HomePage.twitterSocialRed).toHaveAttribute('href', false);
+    await expect(HomePage.instagramSocialRed).toHaveAttribute('href', false);
+  });
+  it('Wait for bodyHome: title, nav and formSignup', async () => {
+    await expect(SignupEmployee.bodySignup).toBeDisplayed();
+    await expect(SignupEmployee.trackgenixNav).toHaveAttribute('href', false);
+    await expect(SignupEmployee.trackgenixNav).toHaveText('TrackGENIX');
+    await expect(SignupEmployee.formSignup).toBeDisplayed();
+  });
+  it('espero que el input tengan su titulo', async () => {
+    await expect(SignupEmployee.firtName).toHaveText('First Name');
+    await expect(SignupEmployee.lastName).toHaveText('Last Name');
+    await expect(SignupEmployee.phone).toHaveText('Phone');
+    await expect(SignupEmployee.email).toHaveText('Email');
+    await expect(SignupEmployee.password).toHaveText('Password');
+  });
+  it('Wait for footer', async () => {
+    await expect(HomePage.footer).toBeDisplayed();
+  });
+});
