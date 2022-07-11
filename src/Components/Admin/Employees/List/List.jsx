c77f5c5525fbcdc045257d6c829be2d78f362825
NancyCima <nancycimabertoni@gmail.com>
2022-07-11 17:57:54 -0300

    TG-114: Sorted the employee list

--- src/Components/Admin/Employees/List/List.jsx
+++ src/Components/Admin/Employees/List/List.jsx
@@ -3,14 +3,17 @@ import Table from 'Components/Shared/Table/Table';
 import { useSelector } from 'react-redux/es/exports';
 
 const List = () => {
-  const employees = useSelector((state) => state.employees.list);
+  const listEmployees = useSelector((state) => state.employees.list);
+  const listActiveEmployees = listEmployees.filter((employee) => employee.active == true);
+  const listInactiveEmployees = listEmployees.filter((employee) => employee.active == false);
+  const employees = listActiveEmployees.concat(listInactiveEmployees);
 
   return (
     <Table
       title={'Employees'}
       data={employees}
-      headersColumns={['ID', 'Fist Name', 'Last Name', 'Phone', 'Email']}
-      headers={['_id', 'first_name', 'last_name', 'phone', 'email']}
+      headersColumns={['Fist Name', 'Last Name', 'Phone', 'Email', 'Status']}
+      headers={['first_name', 'last_name', 'phone', 'email', 'active']}
     />
   );
 };
