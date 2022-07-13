2908fe988569cc952721c35a5a51adffb2e2b1b8
NancyCima <nancycimabertoni@gmail.com>
2022-07-13 19:52:10 -0300

    TG-114: Changed the values of 'active' from booleans to strings

--- src/Components/Admin/Employees/List/List.jsx
+++ src/Components/Admin/Employees/List/List.jsx
@@ -6,12 +6,18 @@ const List = () => {
   const listEmployees = useSelector((state) => state.employees.list);
   const listActiveEmployees = listEmployees.filter((employee) => employee.active == true);
   const listInactiveEmployees = listEmployees.filter((employee) => employee.active == false);
-  const employees = listActiveEmployees.concat(listInactiveEmployees);
+  const employeesSorted = listActiveEmployees.concat(listInactiveEmployees);
+
+  for (let i = 0; i < employeesSorted.length; i++) {
+    employeesSorted[i].active = employeesSorted[i].active.toString();
+  }
+
+  console.log('Sorted', employeesSorted);
 
   return (
     <Table
       title={'Employees'}
-      data={employees}
+      data={employeesSorted}
       headersColumns={['Fist Name', 'Last Name', 'Phone', 'Email', 'Status']}
       headers={['first_name', 'last_name', 'phone', 'email', 'active']}
     />
