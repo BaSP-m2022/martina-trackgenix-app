8a2f38e23a4bfc711a4928b5ac401b025c969cf2
NancyCima <nancycimabertoni@gmail.com>
2022-07-14 17:34:09 -0300

    TG-114: Updated employees so the admin cannot add them

--- src/Components/Admin/Employees/index.js
+++ src/Components/Admin/Employees/index.js
@@ -1,8 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import styles from 'Components/Admin/Employees/employees.module.css';
-import EmployeeForm from 'Components/Admin/Employees/Form/EmployeeForm';
-import Modal from 'Components/Shared/Modal/Modal';
-import Button from 'Components/Shared/Buttons/Buttons';
 import Loader from 'Components/Shared/Loader/Loader';
 import Table from 'Components/Shared/Table/Table';
 import { useDispatch, useSelector } from 'react-redux';
@@ -11,19 +8,6 @@ import { getEmployees } from 'redux/employees/thunks';
 const Employees = () => {
   const dispatch = useDispatch();
   const isLoading = useSelector((state) => state.employees.isLoading);
-
-  const [showForm, setShowForm] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-  const [childrenModal, setChildrenModal] = useState('');
-  const [previewEmployee, setPreviewEmployee] = useState({
-    _id: '',
-    first_name: '',
-    last_name: '',
-    phone: '',
-    email: '',
-    password: '',
-    active: ''
-  });
 
   useEffect(() => {
     dispatch(getEmployees());
@@ -34,34 +18,18 @@ const Employees = () => {
   const listInactiveEmployees = listEmployees.filter((employee) => employee.active == false);
   const employeesSorted = listActiveEmployees.concat(listInactiveEmployees);
 
-  const handleClose = () => {
-    setShowModal(false);
-  };
-
   return (
     <>
       {isLoading ? (
         <Loader show={isLoading} />
       ) : (
         <section className={styles.container}>
-          <EmployeeForm
-            showForm={showForm}
-            setShowForm={setShowForm}
-            setShowModal={setShowModal}
-            setChildrenModal={setChildrenModal}
-            previewEmployee={previewEmployee}
-            setPreviewsEmployee={setPreviewEmployee}
-          />
           <Table
             title={'Employees'}
             data={employeesSorted}
-            headersColumns={['Fist Name', 'Last Name', 'Phone', 'Email', 'Active']}
-            headers={['first_name', 'last_name', 'phone', 'email', 'active']}
+            headersColumns={['Fist Name', 'Last Name', 'Phone', 'Email']}
+            headers={['first_name', 'last_name', 'phone', 'email']}
           />
-          <Button onClick={() => setShowForm(true)}>Add New Employee</Button>
-          <Modal isOpen={showModal} handleClose={handleClose}>
-            {childrenModal}
-          </Modal>
         </section>
       )}
     </>
