56e22cb9e372dba9cbcf69f7d15a8888925c1d7f
SebastianCocci <sebacocci@gmail.com>
2022-07-18 16:46:43 -0300

    TG-116: Greeting at home.

--- src/Components/SuperAdmin/Home/home.jsx
+++ src/Components/SuperAdmin/Home/home.jsx
@@ -1,10 +1,12 @@
 import styles from './home.module.css';
+import { useSelector } from 'react-redux';
 
 function Home() {
+  const user = useSelector((state) => state.auth.user);
   return (
     <section className={styles.container}>
       <div>
-        <h2>Welcome Boss</h2>
+        <h2>{`Welcome ${user.firstName} ${user.lastName}`}</h2>
       </div>
     </section>
   );
