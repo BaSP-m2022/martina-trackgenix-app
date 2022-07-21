e3dcd622ffd4e22967e64a709ad78eeada969ce1
Javier Biselli <javierbiselli@gmail.com>
2022-07-21 01:50:21 -0300

    TG-112: Pulled from master, modified home index to show admin name dynamically

--- src/Components/Admin/Home/index.js
+++ src/Components/Admin/Home/index.js
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react';
 import styles from './home.module.css';
+import firebaseApp from 'helper/firebase/index';
 
 function Home() {
+  const [currentAdmin, setCurrentAdmin] = useState();
+
+  useEffect(() => {
+    firebaseApp.auth().onAuthStateChanged((admin) => {
+      setCurrentAdmin(admin);
+    });
+  }, []);
+
   return (
     <section className={styles.container}>
       <div className={styles.container}>
-        <h2>Welcome Super Admin</h2>
+        {currentAdmin && <h2>Welcome {currentAdmin.displayName}</h2>}
+        {/* I'm trying to show the logged admin name, the code works but
+         currentAdmin.displayName is null, however other data like email for
+         example can be shown (try currentAdmin.email). Maybe we need to add
+         admins names to firebase.*/}
       </div>
     </section>
   );
