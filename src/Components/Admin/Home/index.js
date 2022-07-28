d1f02d4c3eec81dc4d5b5f1c78e73a8ab47b4f62
Shirley1702 <shirleyseaton17@gmail.com>
2022-07-27 22:10:13 -0300

    TG-120: home page for employee and admin

--- src/Components/Admin/Home/index.js
+++ src/Components/Admin/Home/index.js
@@ -1,13 +1,17 @@
 import styles from './home.module.css';
 import { useSelector } from 'react-redux';
+import fondoHome from 'Assets/Imgs/fondoHome.png';
+import logoTrack from 'Assets/Imgs/logoTrackgenix.png';
 
 function Home() {
   const user = useSelector((state) => state.auth.user);
-  console.log(user);
   return (
     <section className={styles.container}>
-      <div className={styles.container}>
-        {user && <h2>Welcome {user?.firstName + ' ' + user?.lastName}</h2>}
+      <img className={styles.fondoHome} src={fondoHome} />
+      <div className={styles.wellcome}>
+        <h2>Wellcome</h2>
+        {user && <h2>W{user?.firstName + ' ' + user?.lastName}</h2>}
+        <img className={styles.logoTrack} src={logoTrack} />
       </div>
     </section>
   );
--- src/Components/Employee/Home/index.js
+++ src/Components/Employee/Home/index.js
@@ -1,11 +1,18 @@
 import styles from './home.module.css';
 import { useSelector } from 'react-redux';
+import fondoHome from 'Assets/Imgs/fondoHome.png';
+import logoTrack from 'Assets/Imgs/logoTrackgenix.png';
 
 function Home() {
   const user = useSelector((state) => state.auth.user);
   return (
     <section className={styles.container}>
-      <div>{user && <h2>Welcome {user?.first_name + ' ' + user?.last_name}</h2>}</div>
+      <img className={styles.fondoHome} src={fondoHome} />
+      <div className={styles.wellcome}>
+        <h2>Wellcome</h2>
+        {user && <h2>{user?.first_name + ' ' + user?.last_name}</h2>}
+        <img className={styles.logoTrack} src={logoTrack} />
+      </div>
     </section>
   );
 }
