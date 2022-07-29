840e4de156e607030ab86b411214e94b664ed04b
Luciano Alarcon <luchialarcon123@gmail.com>
2022-07-29 01:10:53 -0300

    TG-136: hotfix home page on employee

--- src/Components/Employee/Home/index.js
+++ src/Components/Employee/Home/index.js
@@ -5,12 +5,15 @@ import logoTrack from 'Assets/Imgs/logoTrackgenix.png';
 
 function Home() {
   const user = useSelector((state) => state.auth.user);
+  const employeeList = useSelector((state) => state.employees.list);
+
+  const employeeFound = employeeList.find((item) => item?._id === user?._id);
   return (
     <section className={styles.container}>
       <img className={styles.fondoHome} src={fondoHome} />
       <div className={styles.wellcome}>
         <h2>Welcome</h2>
-        {user && <h2>{user?.first_name + ' ' + user?.last_name}</h2>}
+        {employeeFound && <h2>{employeeFound?.first_name + ' ' + employeeFound?.last_name}</h2>}
         <img className={styles.logoTrack} src={logoTrack} />
       </div>
     </section>
